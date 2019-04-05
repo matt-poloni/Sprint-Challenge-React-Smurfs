48271dbae7e37da7049920402bf6dd0c8d36eb25
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-05 12:50:35 -0400

    Creates pages for individual smurfs that can load on refresh

--- village/src/App.js
+++ village/src/App.js
@@ -5,6 +5,7 @@ import { NavLink, Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
 
 class App extends Component {
   constructor(props) {
@@ -44,6 +45,12 @@ class App extends Component {
           exact path="/"
           render={props =>
             <Smurfs {...props} smurfs={this.state.smurfs} />
+          }
+        />
+        <Route
+          exact path="/smurf/:id"
+          render={props =>
+            <Smurf {...props} smurfs={this.state.smurfs} />
           }
         />
         <Route
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+class Smurf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.id,
+      name: this.props.name,
+      height: this.props.height,
+      age: this.props.age,
+    }
+  }
 
-Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  componentDidMount() {
+    if(this.props.match) {
+      axios.get('http://localhost:3333/smurfs')
+        .then(res => {
+          const smurf = res.data.filter(smurf => `${smurf.id}` === this.props.match.params.id)[0]
+          console.log(this.props.match.params.id);
+          this.setState({ ...smurf })
+        })
+        .catch(err => console.log(err.response));
+      }
+    }
+
+  render() {
+    return (
+      <div className="Smurf">
+        <h3>{this.state.name}</h3>
+        <strong>{this.state.height} tall</strong>
+        <p>{this.state.age} smurf years old</p>
+      </div>
+    );
+  }
 };
 
 export default Smurf;
